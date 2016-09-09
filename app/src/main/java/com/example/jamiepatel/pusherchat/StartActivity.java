b4989dbf953d10a38ce89b842b427e4dd4081c08
Kevin Zavier <kzavier@ucsd.edu>
2016-09-08 22:41:50 -0700

    Got the button and logo to show

--- app/src/main/java/com/example/jamiepatel/pusherchat/StartActivity.java
+++ app/src/main/java/com/example/jamiepatel/pusherchat/StartActivity.java
@@ -6,17 +6,22 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by kevin on 8/10/16.
  */
 public class StartActivity extends Activity {
+    Button start;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        start = (Button) findViewById(R.id.button2);
+        start.setBackgroundResource(R.drawable.ic_button_getgoing);
     }
 
     public void onBottonTap(View v){
+
         SmsManager smsManager = SmsManager.getDefault();
         TelephonyManager tm = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
         String number = tm.getLine1Number();
